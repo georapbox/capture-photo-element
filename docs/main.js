e83944ca284b7ae477dbbf41a7644e90642b0b5c
georapbox <georapbox@gmail.com>
2022-03-28 13:48:56 +0300

    Print events in demo console

---
+++ docs/main.js
@@ -0,0 +1,30 @@
+import { CapturePhoto } from 'https://unpkg.com/@georapbox/capture-photo-element/dist/capture-photo.min.js';
+
+const $console = document.getElementById('console');
+
+document.addEventListener('capture-photo:facing-mode-change', evt => {
+  console.log('capture-photo:facing-mode-change ->', evt.detail);
+  $console.innerHTML += `<div>$ <span class="info">capture-photo:facing-mode-change</span> -> ${JSON.stringify(evt.detail)}</div>`;
+});
+
+document.addEventListener('capture-photo:error', evt => {
+  console.log('capture-photo:error ->', evt.detail);
+  $console.innerHTML += `<div>$ <span class="error">capture-photo:error</span> -> ${evt.detail.error.name}: ${evt.detail.error.message}</div>`;
+});
+
+document.addEventListener('capture-photo:success', evt => {
+  console.log('capture-photo:success ->', evt.detail);
+  $console.innerHTML += `<div>$ <span class="success">capture-photo:success</span> -> ${JSON.stringify(evt.detail)}</div>`;
+});
+
+document.addEventListener('capture-photo:camera-resolution-change', evt => {
+  console.log('capture-photo:camera-resolution-change ->', evt.detail);
+  $console.innerHTML += `<div>$ <span class="info">capture-photo:camera-resolution-change</span> -> ${JSON.stringify(evt.detail)}</div>`;
+});
+
+document.addEventListener('capture-photo:zoom-change', evt => {
+  console.log('capture-photo:zoom-change ->', evt.detail);
+  $console.innerHTML += `<div>$ <span class="info">capture-photo:zoom-change</span> -> ${JSON.stringify(evt.detail)}</div>`;
+});
+
+CapturePhoto.defineCustomElement();
