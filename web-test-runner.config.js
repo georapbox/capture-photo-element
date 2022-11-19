d9e99a8db7c3566dadacf136fb7179bb76f2ac4a
georapbox <georapbox@gmail.com>
2022-11-19 18:03:28 +0200

    clean deps install

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -3,11 +3,10 @@ import { playwrightLauncher } from '@web/test-runner-playwright';
 export default {
   rootDir: '.',
   files: 'test/*.test.js',
-  concurrentBrowsers: 3,
+  concurrentBrowsers: 2,
   nodeResolve: true,
   browsers: [
     playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'firefox' }),
-    playwrightLauncher({ product: 'webkit' })
+    playwrightLauncher({ product: 'firefox' })
   ]
 };
