c28df9d40de7523c47df15d262474398ce3e418f
George Raptis <georapbox@gmail.com>
2024-08-21 19:06:55 +0300

    Update demo

---
+++ docs/js/utils.js
@@ -0,0 +1,39 @@
+/**
+ * Converts the characters "&", "<", ">", '"' and "'" in a string to their corresponding HTML entities.
+ *
+ * @param {String} subjectString The HTML string to escape.
+ * @throws {TypeError} If `subjectString` is not string.
+ * @returns {String} Returns the escaped string.
+ * @example
+ *
+ * escapeHTML('<script src="main.js"></script>');
+ * // => '&lt;script src=&quot;main.js&quot;&gt;&lt;/script&gt;'
+ *
+ * escapeHTML('Hello & <span>World</span>');
+ * // => 'Hello &amp; &lt;span&gt;World&lt;/span&gt;'
+ *
+ * escapeHTML('<p data-id="1">lorem ipsum</p>');
+ * // => '&lt;p data-id=&quot;1&quot;&gt;lorem ipsum&lt;/p&gt;'
+ */
+function escapeHTML(subjectString) {
+  if (typeof subjectString !== 'string') {
+    throw new TypeError('Expected a string for first argument');
+  }
+
+  const htmlEscapes = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;'
+  };
+
+  const regexUnescapedHtml = /[&<>"'`]/g;
+  const regexHasUnescapedHtml = RegExp(regexUnescapedHtml.source);
+
+  return regexHasUnescapedHtml.test(subjectString)
+    ? subjectString.replace(regexUnescapedHtml, tag => htmlEscapes[tag] || tag)
+    : subjectString;
+}
+
+export { escapeHTML };
